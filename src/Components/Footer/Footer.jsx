b1dcd4848f24f0876a46bdbfc8d45ff9b702482c
Jay Sullivan <JaySull514@gmail.com>
2018-03-15 14:14:28 -0500

    Portfolio link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,13 @@ import './Footer.css';
 function Footer() {
   return (
     <footer>
-      <p>&copy; Jacob Sullivan 2017</p>
+      <p>
+        &copy;{' '}
+        <a className="footer-link" href="http://www.thejaysully.com/" target="_blank">
+          Jacob Sullivan
+        </a>{' '}
+        2018
+      </p>
       <a href="https://www.github.com/Jaygles/" target="_blank">
         <img className="footer-logo" src="assets/img/github-icon.png" alt="github logo" />
       </a>
